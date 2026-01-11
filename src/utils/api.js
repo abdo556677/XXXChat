2f3a384b8140b12ee48e22fd72a04a88b9efb39e
abdo556677 <abdomoha121212@gmail.com>
2026-01-11 17:32:21 +0100

    Initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,49 +4,50 @@ import { useAuthStore } from '@/stores/auth'
 
 // إنشاء instance لـ axios
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+    baseURL: 'https://spectacular-dorey-takami-97ae07ca.koyeb.app/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 })
 
 // === Request interceptor: إضافة التوكن لكل طلب ===
 api.interceptors.request.use(
-  (config) => {
-    try {
-      const authStore = useAuthStore()
-      const accessToken = authStore.accessToken  // نفترض أن authStore يحتوي على accessToken
-      if (accessToken) {
-        config.headers.Authorization = accessToken
-        // console.log('Added Authorization header from Pinia:', accessToken)
-      }
-    } catch (err) {
-      console.warn('Auth store not ready', err)
-    }
-    return config
-  },
-  (error) => Promise.reject(error)
+    config => {
+        try {
+            const authStore = useAuthStore()
+            const accessToken = authStore.accessToken // نفترض أن authStore يحتوي على accessToken
+            if (accessToken) {
+                config.headers.Authorization = accessToken
+                // console.log('Added Authorization header from Pinia:', accessToken)
+            }
+        } catch (err) {
+            console.warn('Auth store not ready', err)
+        }
+        return config
+    },
+    error => Promise.reject(error),
 )
 // === Response interceptor: التعامل مع 401 بطريقة آمنة ===
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const originalRequest = error.config
+    response => response,
+    error => {
+        const originalRequest = error.config
 
-    // استثناء endpoint logout لتجنب حلقة لانهائية
-    if (originalRequest?.url?.includes('/auth/logout')) {
-      return Promise.reject(error)
-    }
+        // استثناء endpoint logout لتجنب حلقة لانهائية
+        if (originalRequest?.url?.includes('/auth/logout')) {
+            return Promise.reject(error)
+        }
 
-    // إذا 401 ولم يُعالج من قبل
-    if (error.response?.status == 401 || error.response?.status == 403 ) { //&& !originalRequest._retry
-      originalRequest._retry = true
-      const authStore = useAuthStore()
-      authStore.logout() // تسجيل خروج بدون loop
-    }
+        // إذا 401 ولم يُعالج من قبل
+        if (error.response?.status == 401 || error.response?.status == 403) {
+            //&& !originalRequest._retry
+            originalRequest._retry = true
+            const authStore = useAuthStore()
+            authStore.logout() // تسجيل خروج بدون loop
+        }
 
-    return Promise.reject(error)
-  }
+        return Promise.reject(error)
+    },
 )
 
 export default api
